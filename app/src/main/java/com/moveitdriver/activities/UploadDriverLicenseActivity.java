2234f8f8146a76dfa03d28464809f529c3826740
salman1994 <msalmanjavaid11@gmail.com>
2019-02-18 16:08:42 +0500

    Latest Commit...

--- app/src/main/java/com/moveitdriver/activities/UploadDriverLicenseActivity.java
+++ app/src/main/java/com/moveitdriver/activities/UploadDriverLicenseActivity.java
@@ -1,17 +1,66 @@
 package com.moveitdriver.activities;
 
+import android.Manifest;
+import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.moveitdriver.R;
+import com.moveitdriver.models.getAllVehicleResponse.Datum;
+import com.moveitdriver.models.getAllVehicleResponse.GetAllVehicleModelResponse;
+import com.moveitdriver.retrofit.RestHandler;
+import com.moveitdriver.retrofit.RetrofitListener;
+import com.moveitdriver.utils.Constants;
+import com.moveitdriver.utils.FileUtils;
+import com.moveitdriver.utils.ImagePicker;
 import com.moveitdriver.utils.SharedPrefManager;
 
-public class UploadDriverLicenseActivity extends AppCompatActivity implements View.OnClickListener{
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Response;
+
+public class UploadDriverLicenseActivity extends AppCompatActivity implements View.OnClickListener, RetrofitListener {
+
+    private EditText driverLicenceExpiresEditText, validDriverLicenceEditText;
+    private Calendar myCalendar;
+
+    private ProgressDialog pDialog;
+    private RestHandler restHandler;
+
+    private static final int REQUEST_READ_STORAGE = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +69,19 @@ public class UploadDriverLicenseActivity extends AppCompatActivity implements Vi
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        // Declare RestHandler...
+        restHandler = new RestHandler(this, this);
+
+        // Declare Progress Dialog...
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Loading...");
+        pDialog.setCancelable(false);
+
+        DatePickerIns();
     }
+
+    // ------------------------------ Override Functions  --------------------------------------- //
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -46,8 +107,74 @@ public class UploadDriverLicenseActivity extends AppCompatActivity implements Vi
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
 
+        }
+    }
+
+    @Override
+    public void onSuccess(Call call, Response response, String method) {
+        if (response != null && response.code() == 200) {
+            if (method.equalsIgnoreCase("getAllVehicles")) {
+                pDialog.dismiss();
+            }
+        } else if (response != null && (response.code() == 403 || response.code() == 500)) {
+            if (pDialog != null && pDialog.isShowing()) {
+                pDialog.dismiss();
+            }
+
+            try {
+                ResponseBody body = response.errorBody();
+                JSONObject jObj = new JSONObject(body.string());
+                if (jObj.optString("status").equals("403"))
+                    Constants.showAlert(this, jObj.optString("message"));
+                else
+                    Constants.showAlert(this, jObj.optString("message"));
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            } catch (JSONException e1) {
+                Constants.showAlert(this, "Oops! API returned invalid response. Try again later.");
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onFailure(String errorMessage) {
+        if (pDialog != null && pDialog.isShowing()) {
+            pDialog.dismiss();
+        }
+        Constants.showAlert(this, errorMessage);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_READ_STORAGE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                proceedToImagePicking(imageCode);
+            }
+        } else
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case 1:
+//                Bitmap fBitmap = ImagePicker.getImageFromResult(this, resultCode, data);
+//                if (fBitmap != null)
+//                    vehicleFrontImageView.setImageBitmap(fBitmap);
+//                carFrontImageUri = getImageUri(this, fBitmap);
+//                break;
+//            case 2:
+//                Bitmap bBitmap = ImagePicker.getImageFromResult(this, resultCode, data);
+//                if (bBitmap != null)
+//                    vehicleBackImageView.setImageBitmap(bBitmap);
+//                carBackImageUri = getImageUri(this, bBitmap);
+//                break;
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+                break;
         }
     }
 
@@ -55,5 +182,128 @@ public class UploadDriverLicenseActivity extends AppCompatActivity implements Vi
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    // ---------------------------------   FUNCTIONS   -----------------------------------------  //
+
+    private void getCarModels(String makeId) {
+        pDialog.show();
+        restHandler.makeHttpRequest(restHandler.retrofit.create(RestHandler.RestInterface.class).getModels(makeId), "getModels");
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void addVehicleDetail() {
+        pDialog.show();
+
+        MultipartBody.Part fImage = null; // Vehicle Front Image
+        MultipartBody.Part bImage = null; // Vehicle Back Image
+
+//        fImage = prepareFilePart("carFrontPic", licenceFrontImageUri);
+//        bImage = prepareFilePart("carBackPic", licenceBackImageUri);
+//
+//        restHandler.makeHttpRequest(restHandler.retrofit.create(RestHandler.RestInterface.class).addVehicleDetail(RequestBody.create(MediaType.parse("text/plain"), SharedPrefManager.getInstance(this).getDriverId()),
+//                RequestBody.create(MediaType.parse("text/plain"), selectedCarMake),
+//                RequestBody.create(MediaType.parse("text/plain"), selectedCarModel),
+//                RequestBody.create(MediaType.parse("text/plain"), selectedCarYear),
+//                RequestBody.create(MediaType.parse("text/plain"), selectedCarColor),
+//                fImage,
+//                bImage),
+//                "addDriverLicenceDetail");
+    }
+
+    private int imageCode;
+
+    public void onPickImage(int code) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                proceedToImagePicking(code);
+            } else {
+                imageCode = code;
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_READ_STORAGE);
+            }
+        } else {
+            proceedToImagePicking(code);
+        }
+    }
+
+    private void proceedToImagePicking(int code) {
+        Intent chooseImageIntent = ImagePicker.getPickImageIntent(this);
+        startActivityForResult(chooseImageIntent, code);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @NonNull
+    private MultipartBody.Part prepareFilePart(String partName, Uri fileUri) {
+        File file = FileUtils.getFile(this, fileUri);
+        MediaType type = MediaType.parse(getMimeType(fileUri));
+        RequestBody requestFile = RequestBody.create(type, file);
+        return MultipartBody.Part.createFormData(partName, file.getName(), requestFile);
+    }
+
+    public String getMimeType(Uri uri) {
+        String mimeType = null;
+        if (uri.getScheme().equals(ContentResolver.SCHEME_CONTENT)) {
+            ContentResolver cr = getApplicationContext().getContentResolver();
+            mimeType = cr.getType(uri);
+        } else {
+            String fileExtension = MimeTypeMap.getFileExtensionFromUrl(uri.toString());
+            mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(fileExtension.toLowerCase());
+        }
+        return mimeType;
+    }
+
+    private Uri getImageUri(Context context, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
+    }
+
+    public void DatePickerIns() {
+        myCalendar = Calendar.getInstance();
+
+        driverLicenceExpiresEditText = (EditText) findViewById(R.id.driver_licence_expires_date_upload_document_activity);
+        validDriverLicenceEditText = (EditText) findViewById(R.id.valid_driver_licence_date_upload_document_activity);
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+        };
+
+        driverLicenceExpiresEditText.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(UploadDriverLicenseActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        validDriverLicenceEditText.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(UploadDriverLicenseActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    private void updateLabel() {
+        String myFormat = "yyyy-mm-dd"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        driverLicenceExpiresEditText.setText(sdf.format(myCalendar.getTime()));
     }
 }
